1770ccc97ff61293a6344a6da9838e30f5173e88
Yeabsira Moges <yabume123@gmail.com>
2025-05-01 17:07:15 +0300

    Created 'Seat Reservation Manager' by A2SV Hub

---
+++ 01-May-2025/Seat Reservation Manager 346442.py
@@ -0,0 +1,15 @@
+# Problem: Seat Reservation Manager - https://leetcode.com/problems/seat-reservation-manager/description/
+
+class SeatManager:
+    def __init__(self, n: int):
+        self.heap = list(range(1, n + 1))
+        heapify(self.heap)        
+
+    def reserve(self) -> int:
+        return heappop(self.heap)
+
+    def unreserve(self, seatNumber: int) -> None:
+        heappush(self.heap, seatNumber)
+
+
+# Is you kidding
