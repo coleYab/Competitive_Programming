d197a208b8a731f0dc24238f07c7e0d9e76e383f
Yeabsira Moges <yabume123@gmail.com>
2025-09-24 16:58:01 +0300

    Created 'N-th Tribonacci Number' by A2SV Hub

---
+++ 24-Sep-2025/N-th Tribonacci Number 395700.py
@@ -0,0 +1,12 @@
+# Problem: N-th Tribonacci Number - https://leetcode.com/problems/n-th-tribonacci-number/description/
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [0] * (n + 10)
+        dp[1] = 1
+        dp[2] = 1
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+        return dp[n]
